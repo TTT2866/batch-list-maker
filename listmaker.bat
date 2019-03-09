47522a1fe02e536dc0924b32c5108a924e4b1aa5
TTT2866 <41979926+TTT2866@users.noreply.github.com>
2019-03-09 19:48:31 +1100

    Update listmaker.bat
    Cut down a lot of lines, removed reloader.bat function

--- listmaker.bat
+++ listmaker.bat
@@ -1,5 +1,5 @@
 @echo off
-title List thing
+title List maker
 color f
 set /a num=0
 
@@ -10,32 +10,15 @@ set /p title=:
 set title2=%title: =_%
 cls
 echo ========================== >>%userprofile%\desktop\%title2%.txt
-echo %title% >>%userprofile%\desktop\%title2%.txt
+echo %title%.txt >>%userprofile%\desktop\%title2%.txt
 echo ========================== >>%userprofile%\desktop\%title2%.txt
 
-if exist %userprofile%\desktop\reloader.bat (del %userprofile%\desktop\reloader.bat)
-goto reloader
-:reloaded
-
 :add
-set /a num=%num%+1
 cls
-echo.
-echo Something to add to the list
-set /p add=: 
+set /a num=%num%+1
+type %userprofile%\desktop\%title2%.txt
+::set /a num1=%num%+1
+set /p add=%num%: 
 echo %num% - %add% >>%userprofile%\desktop\%title2%.txt
 cls
 goto add
-
-:reloader
-(
-echo @echo off
-echo mode 40,40
-echo :here
-echo type %%userprofile%%\desktop\%title2%.txt
-echo @ping.exe 127.0.0.1 -n 2 -w 2000 ^> nul
-echo cls
-echo goto here
-) >%userprofile%\Desktop\reloader.bat
-start "" %userprofile%\desktop\reloader.bat
-goto reloaded
