091e1e319b5e8eb4b52b01e3f338d8fd417e9f66
TTT2866 <41979926+TTT2866@users.noreply.github.com>
2019-03-09 19:56:19 +1100

    Update listmaker.bat
    Made it smaller and better

--- listmaker.bat
+++ listmaker.bat
@@ -1,24 +1,25 @@
 @echo off
 title List maker
 color f
+mode 60,10
 set /a num=0
 
 cls
 echo.
+set destination=%cd%
 echo List title
 set /p title=: 
 set title2=%title: =_%
 cls
-echo ========================== >>%userprofile%\desktop\%title2%.txt
-echo %title%.txt >>%userprofile%\desktop\%title2%.txt
-echo ========================== >>%userprofile%\desktop\%title2%.txt
+echo ========================== >>%destination%\%title2%.txt
+echo %title%.txt >>%destination%\%title2%.txt
+echo ========================== >>%destination%\%title2%.txt
 
 :add
 cls
 set /a num=%num%+1
-type %userprofile%\desktop\%title2%.txt
-::set /a num1=%num%+1
+type %destination%\%title2%.txt
 set /p add=%num%: 
-echo %num% - %add% >>%userprofile%\desktop\%title2%.txt
+echo %num% - %add% >>%destination%\%title2%.txt
 cls
 goto add
